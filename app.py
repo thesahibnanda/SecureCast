4292885c5d090831b1080cdb11de878671a15527
Sahib Nanda <essenbeats@gmail.com>
2024-12-03 19:43:46 +0530

    Changed Approach

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ class OTPRequest(BaseModel):
     
 class OTPValidationRequest(BaseModel):
     otp: str
+    otp_hash: str
 
 @app.post("/init-otp")
 async def init_otp(request: OTPRequest):
@@ -35,23 +36,18 @@ async def init_otp(request: OTPRequest):
         hashed_otp = HashUtils.hash_string(otp)
         response  = JSONResponse(content={"error": False, "message": "OTP sent successfully"})
         MailUtils.send_mail(Config.SENDER_EMAIL, Config.SENDER_PASSWORD, request.email, Config.SUBJECT, Config.MESSAGE + otp)
-        response.set_cookie(
-                key="otp_hash", 
-                value=hashed_otp, 
-                max_age=Config.MAX_AGE, 
-                secure=False, 
-                samesite="lax",
-                httponly=False)
+        response.headers["otp_hash"] = hashed_otp
         return response
     except Exception as e:
         return JSONResponse(content={"error": True, "message": str(e)})
 
 @app.post("/validate-otp")
-async def validate_otp(request: OTPValidationRequest, otp_hash: Optional[str] = Cookie(None)):
+async def validate_otp(request: OTPValidationRequest):
     try:
-        if not otp_hash:
+        
+        if not request.otp_hash or request.otp_hash == "":
             return JSONResponse(content={"error": True, "valid": False, "message": "OTP has expired or not set"})
-        is_valid = HashUtils.validate_hash(request.otp, otp_hash)
+        is_valid = HashUtils.validate_hash(request.otp, request.otp_hash)
         
         if is_valid:
             return JSONResponse(content={"error": False, "valid": True, "message": "OTP is valid"})
