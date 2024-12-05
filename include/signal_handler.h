d10b7613ce4805b4f378d9c0380b29363dae1210
Sahib Nanda <essenbeats@gmail.com>
2024-12-05 11:41:56 +0530

    moving

--- include/signal_handler.h
+++
@@ -1,6 +0,0 @@
-#ifndef SIGNAL_HANDLER_H
-#define SIGNAL_HANDLER_H
-
-void signal_handler(int signum);
-
-#endif
--- src/signal_handler.cpp
+++
@@ -1,7 +0,0 @@
-#include <signal.h>
-#include <spdlog/spdlog.h>
-
-void signal_handler(int signum) {
-    spdlog::info("Interrupt signal ({}) received. Shutting down...", signum);
-    exit(0);
-}
